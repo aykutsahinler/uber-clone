46955e3eb7785c99fcb5725fc47a70712d8d442f
Numan Aykut Şahinler <nsahinler@anadolusigorta.com.tr>
2024-12-17 14:32:04 +0300

    Add welcome page skip button

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
 
 const Onboarding = () => {
   return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace('/(auth)/sign-up');
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
