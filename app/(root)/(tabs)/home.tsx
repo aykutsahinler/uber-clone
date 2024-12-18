a0164ba8eb32a7d8f468b0509b9f43e8f021ad2e
Numan Aykut Şahinler <nsahinler@anadolusigorta.com.tr>
2024-12-18 16:56:31 +0300

    Add database for login user

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,35 +1,15 @@
-// import { Text, View } from 'react-native';
-
-// const Home = () => {
-//   return (
-//     <View>
-//       <Text>Home</Text>
-//     </View>
-//   );
-// };
-
-// export default Home;
-
-import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
-import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { SignedIn, useUser } from '@clerk/clerk-expo';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Page() {
   const { user } = useUser();
 
   return (
-    <View>
+    <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    </SafeAreaView>
   );
 }
